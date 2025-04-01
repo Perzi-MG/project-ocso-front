d631c4ee80e27fac40d03d4231745cfb51d826c7
Perzi-MG <mgarciammej@gmail.com>
2025-04-01 16:42:38 -0600

    feat: Cookies in SSC

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image"
 
 export default function AuthLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <div className="bg-orange-200 w-screen h-screen overflow-hidde grid">
-        <div className="place-content-center place-self-center text-center">
-          <Image
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="bg-orange-200 w-screen h-screen overflow-hidde grid">
+      <div className="place-content-center place-self-center text-center">
+        <Image
           src="/Oxxo_Logo.svg"
           alt="Logo de Ocso"
           width={250}
           className="place-self-center bottom-10 relative"
-          height={0}/>
-          {children}
-        </div>
-        </div>
-    )
-  }
-  
+          height={0} />
+        {children}
+      </div>
+    </div>
+  )
+}
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,34 +1,44 @@
 "use client";
 import { API_URL } from "@/constants";
-import { Input, Button, user } from "@heroui/react";
+import { Input, Button} from "@heroui/react";
 import axios from "axios";
 import Link from "next/link";
-export default function LoginPage(){
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+export default function LoginPage() {
+    const [submitting, setSubmitting] = useState(false)
+    const router = useRouter()
     const handleSubmit = async (e: React.FormEvent) => {
+        setSubmitting(true);
         e.preventDefault()
         const formData = new FormData(e.target);
         let authData: any = {}
         authData.userEmail = formData.get("UserEmail");
         authData.userPassword = formData.get("UserPassword");
-        const {data} = await axios.post(`${API_URL}/auth/login`, {
-            ...authData
-        }, {
-            withCredentials: true,
-        })
-        console.log(data);
+        try {
+            const response = await axios.post(`${API_URL}/auth/login`, {
+                ...authData
+            }, {
+                withCredentials: true,
+            });
+            if (response.status === 201) router.push('/dashboard')
+            setSubmitting(false);
+        } catch (e) {
+            setSubmitting(false);
+        }
         return;
     }
     return (
         <form className="bg-orange-500 px-10 py-2 rounded-md" onSubmit={handleSubmit}>
-        <p className="text-2xl my-4 text-white">Iniciar Sesión<span></span></p>
-        <div className="flex flex-col gap-2 my-4 items-center">
-            <Input label= "Email" name= "UserEmail" type="email" isRequired= {true} size="sm"/>
-            <Input label= "Contraseña" name= "UserPassword" type="password" isRequired= {true} size="sm"/>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-            <Button color = "primary" type="submit">Iniciar Sesión</Button>
-            <p className="text-white">No tienes cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link></p>
-        </div>
-    </form>
+            <p className="text-2xl my-4 text-white">Iniciar Sesión<span></span></p>
+            <div className="flex flex-col gap-2 my-4 items-center">
+                <Input label="Email" name="UserEmail" type="email" isRequired={true} size="sm" />
+                <Input label="Contraseña" name="UserPassword" type="password" isRequired={true} size="sm" />
+            </div>
+            <div className="flex flex-col items-center gap-2">
+                <Button color="primary" type="submit" disabled={submitting}>{submitting ? "Enviando..." : "Iniciar Sesión"}</Button>
+                <p className="text-white">No tienes cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link></p>
+            </div>
+        </form>
     );
 }
