6a0f39e8c52b1f8a9dca7650bd02cbe66e224f85
Perzi-MG <mgarciammej@gmail.com>
2025-04-04 12:34:39 -0600

    feat: React cache

--- app/dashboard/@locations/_components/LocationCard.tsx
+++ app/dashboard/@locations/_components/LocationCard.tsx
@@ -1,16 +1,15 @@
-import { API_URL, TOKEN_NAME } from "@/constants";
+import { API_URL } from "@/constants";
 import { Location } from "@/entities";
+import { authHeathers } from "@/helpers/authHeaders";
 import { Card, CardBody, CardHeader, Divider } from "@heroui/react";
 import axios from "axios";
-import { cookies, headers } from "next/headers";
 import Link from "next/link";
 
 export default async function LocationCard({ store }: { store: string | string[] | undefined }) {
     if (!store) return null;
-    const token = cookies().get(TOKEN_NAME)?.value;
     const { data } = await axios.get<Location>(`${API_URL}/locations/${store}`, {
         headers: {
-            Authorization: `Bearer ${token}`
+            ...authHeathers()
         }
     }
     )
--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -1,40 +1,42 @@
 import axios from "axios";
-import { cookies } from "next/headers";
-import { API_URL, TOKEN_NAME } from "@/constants";
+import { API_URL } from "@/constants";
 import { Location } from "@/entities";
 import SelectLocation from "./_components/SelectLocation";
 import LocationCard from "./_components/LocationCard";
 import FormNewLocation from "./_components/FormNewLocation";
-const LocationsPage = async ({searchParams}: {
-    searchParams: {[key: string]: string | string[] | undefined};
+import DeleteLocationButton from "./_components/DeleteLocationButton";
+import { authHeathers } from "@/helpers/authHeaders";
+const LocationsPage = async ({ searchParams }: {
+    searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-    const userCookies = cookies();
-    const token = userCookies.get(TOKEN_NAME)?.value
     let { data } = await axios.get<Location[]>(`${API_URL}/locations`, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            ...authHeathers()
         }
     });
     data = [
         {
-        locationId: 0,
-        locationName: "Ninguna",
-        locationLatLng: [0, 0],
-        locationAddress: "No existe",
-    },
+            locationId: 0,
+            locationName: "Ninguna",
+            locationLatLng: [0, 0],
+            locationAddress: "No existe",
+        },
         ...data,
-]
+    ]
 
     return (
         <div className="w-8/12">
             <div className="w-full flex flex-col items-center h-[90vh] bg-red-50">
                 <div className="w-1/2 my-10">
-                    <SelectLocation locations={data} store={searchParams?.store}/>
+                    <SelectLocation locations={data} store={searchParams?.store} />
                 </div>
                 <div className="w-8/12">
-                    <LocationCard store={searchParams.store}/>
+                    <LocationCard store={searchParams.store} />
                 </div>
-                <FormNewLocation/>
+                <div className="w-6/12">
+                    <FormNewLocation store={searchParams.store}/>
+                </div>
+                <DeleteLocationButton store = {searchParams.store}/>
             </div>
         </div>
     );
