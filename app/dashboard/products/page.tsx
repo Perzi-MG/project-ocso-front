6761f6093c4ae73b808c27fdcabf9710cc092116
Perzi-MG <mgarciammej@gmail.com>
2025-04-12 18:25:11 -0600

    Barra de Busqueda Implementada

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,27 @@
-const ProductsPage = () => {
-    return null;
+import { API_URL } from "@/constants";
+import { Product } from "@/entities";
+import { authHeathers } from "@/helpers/authHeaders";
+import ProductCard from "./_components/ProductCard";
+import Link from "next/link";
+import FilteredCards from "./_components/FilteredCards";
+
+const ProductsPage = async () => {
+    const response = await fetch(`${API_URL}/products`, {
+        headers: {
+            ...authHeathers(),
+        },
+        next: {
+            tags: ["dashboard:products"]
+        }
+    });
+    const products: Product[] = await response.json();
+    return (
+        <div className="h-[90vh] w-full">
+            <div className="w-3/12">
+                <FilteredCards products={products} />
+            </div>
+        </div>
+    );
 }
 
 export default ProductsPage;
